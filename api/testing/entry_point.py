06cb0393482badeb97ea8d52eb670b76516cd389
Sloimayyy <sloimayyy@gmail.com>
2023-09-22 03:53:33 +0200

    added basic ROM generator

--- api/testing/entry_point.py
+++ api/testing/entry_point.py
@@ -5,6 +5,7 @@ import sys
 
 from generators.generator import Generator
 from generators.generator_repository.adders import FiveHertzAdder
+from generators.generator_repository.roms import BasicROMGenerator
 
 working_path = str(pathlib.Path(__file__).resolve().parents[1])
 sys.path.append(working_path)
@@ -32,7 +33,20 @@ def main():
     #FiveHertzYDecoder.generate(chiseled_bookshelves=False).save(r'C:\Users\Bananas Man\AppData\Roaming\.minecraft\config\worldedit\schematics',
     #                                  'test1', Version.JE_1_19_4)
     #FiveHertzYDecoder.get_metadata()
-    FiveHertzAdder.generate(bit_count=8).save(
+    #FiveHertzAdder.generate(bit_count=8).save(
+    #    r'C:\Users\Bananas Man\AppData\Roaming\.minecraft\config\worldedit\schematics',
+    #                                  'test1', Version.JE_1_19_4)
+
+    BasicROMGenerator.generate(data='0123456789ABCDEF8721364778635867923576862467857862678456784'*100,
+                               base=16,
+                               bit_width=4,
+                               x_offsets=[2,],
+                               z_offsets=[2, 3, 5, 8],
+                               x_word_count=16,
+                               z_word_count=16,
+                               x_stagger='even',
+                               z_stagger='even',
+                               stagger_intersection_mode='xor').save(
         r'C:\Users\Bananas Man\AppData\Roaming\.minecraft\config\worldedit\schematics',
                                       'test1', Version.JE_1_19_4)
 
